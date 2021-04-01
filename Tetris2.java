a02fe4ba4f48f914cfe17503076f763684962dfb
Helen_Maher <helenhmaher@gmail.com>
2021-04-01 15:25:30 +0200

    tetris step 4

--- Tetris2.java
+++ Tetris2.java
@@ -1,7 +1,7 @@
 /**************************************
  * File: Tetris1.java Author: helen maher Walk-through: http://java.macteki.com/
  * 
- * Description: RED CELL
+ * Description: Step 4 draw a token
  *************************************/
 
 class Tetris2 extends javax.swing.JPanel {
@@ -15,16 +15,23 @@ class Tetris2 extends javax.swing.JPanel {
         this.setBackground(java.awt.Color.GREEN);
     }
 
-    public void paint(java.awt.Graphics gr) {
-        super.paint(gr);
+    public void drawCell(java.awt.Graphics gr, int x, int y) {
         gr.setColor(java.awt.Color.BLACK);
         gr.fillRect(0, 0, 24, 24);
         gr.setColor(java.awt.Color.RED);
         gr.fillRect(1, 1, 22, 22);
     }
 
+    public void paint(java.awt.Graphics gr) {
+        super.paint(gr);
+        drawCell(gr, 0, 0);
+        drawCell(gr, 0, 1 * 24);
+        drawCell(gr, 1 * 24, 0);
+        drawCell(gr, 2 * 24, 0);
+    }
+
     public static void main(String[] args) throws Exception {
-        javax.swing.JFrame window = new javax.swing.JFrame("Maher Tetris");
+        javax.swing.JFrame window = new javax.swing.JFrame("Tetris");
         window.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 
         Tetris2 tetris = new Tetris2();
